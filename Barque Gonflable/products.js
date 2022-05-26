56e6e6388f1d29d177639041025a574d95abb63a
billel01 <106335815+billel01@users.noreply.github.com>
2022-05-26 21:20:17 +0200

    Add files via upload

---
+++ Barque Gonflable/products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: '  قارب متين لشخصين و طفل صغير مثالي للاستمتاع بأجواء الصيف و بكل أمان  Bestway Barque Gonflable avec Une Pompe et 2 Rames 232cmx115cm ',
+		price1: 12300,
+		price: 10800,
+		description: ' عيش اللحظة و لمة العايلة والاحباب مع البرادة المحمولة 4في1 و تمتع بمأكولاتك طازة و باردة أثناء رحلاتك التخييمية ، سهلة التنقل ما يجعلها من القطع الأساسية في كل خرجات . اغتنم الفرصة و أطلب الأن.'   ,
+		phone: 0770439221,
+	},
+
+	
+];
---
+++ Offre 3en1/products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: '  حصريا 3في1 خيمة لأربعة أشخاص مع فرشة هوائية و مصباح الرأس بسعر مغري  Exclusif ! 3en1 Tente 4 Places Avec Matela et Lampe frontale à un prix attractif ',
+		price1: 8900,
+		price: 6555,
+		description: ' هل أنت من عشاق المغامرة و الرحلات تريد تشكيل مجموعتك الخاصة للتخييم ؟ أنت في الوقت و المكان المناسب . أحصل الان على طقم التخييم 3في1 الذي يحتوي على خيمة خفيفة الوزن و سهلة التجهيز. فرشة هوائية تمنحك راحة و نوم عميق . مصباح الرأس بدرجة سطوع عالية مجانا. ماذا تنتظر ؟إغتنم الفرصة و قدم طلبك قبل نفاذ الكمية'   ,
+		phone: 0770439221,
+	},
+
+	
+];
