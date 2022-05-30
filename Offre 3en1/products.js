21822a85cffe639a140d8f06b6d55083c0b1dfa5
billel01 <106335815+billel01@users.noreply.github.com>
2022-05-30 13:46:09 +0200

    Update products.js

--- Offre 3en1/products.js
+++ Offre 3en1/products.js
@@ -7,7 +7,7 @@ const products = [
 		image3: 'img/products/3.png',
 		image4: 'img/products/4.png',
 		image5: 'img/products/5.png',
-		title: '  حصريا 3في1 خيمة لأربعة أشخاص مع فرشة هوائية و مصباح متعدد الاستعمالات بسعر مغري  Exclusif ! 3en1 Tente 4 Places Avec Matela et Lampe Multifonctions à un prix attractif ',
+		title: '  عرض خاص خيمة لأربعةأشخاص مع فرشة هوائية ومصباح متعدد الاستعمالات بسعر مغري  Exclusif ! 3en1 Tente 4 Places Avec Matela et Lampe Multifonctions à un prix attractif ',
 		price1: 8800,
 		price: 7300,
 		description: ' هل أنت من عشاق المغامرة و الرحلات و تريد تشكيل مجموعتك الخاصة للتخييم ؟ أنت في الوقت و المكان المناسب . أحصل الان على طقم التخييم 3في1 الذي يحتوي على خيمة خفيفة الوزن و سهلة التجهيز. فرشة هوائية تمنحك راحة و نوم عميق . مصباح متعدد الاستعمالات 13في1 بدرجة سطوع عالية مجانا. ماذا تنتظر ؟إغتنم الفرصة و قدم طلبك قبل نفاذ الكمية'   ,
